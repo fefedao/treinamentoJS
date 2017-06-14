39b23f924574fe52e59465f8595b9a69c34b6c1c
Luiz Hartke <luiz.hartke@gmail.com>
2017-06-13 22:11:14 -0300

    Adiciona exercicio 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Ex 1
 let command = "create table author (id number, name string, age number, city string, state string, country string)";
 let exp = /create table ([a-z]+) \(([a-z\s,]+)\)/;
 let evaluatedExp = exp.exec(command)
@@ -5,3 +6,24 @@ let tableName = evaluatedExp[1];
 let columns = evaluatedExp[2];
 columns = columns.split(/,\s/);
 console.log(tableName, columns);
+
+//Ex 2
+let database = {
+    tables : {
+    }
+
+};
+
+database.tables[tableName] = {
+    columns : {}
+};
+
+columns.forEach(column =>{
+    let [name,value] = column.split(" ");
+    database.tables[tableName].columns[name] = value;
+    console.log(database.tables[tableName].columns)
+});
+
+console.log(JSON.stringify(database));
+
+
